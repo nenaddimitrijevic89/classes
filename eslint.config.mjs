6fa2b01b219fd162ffde0d193f2b1c6313771b80
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2025-03-07 18:25:57 +0100

    update eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      // Add your custom rules here
+    },
+  },
 ]
 
 export default eslintConfig
